38db7d2db93b6d851b2701e14aeb0209f1c7e1b2
jer bartels <jer@satch.cc>
2017-05-15 18:31:09 +0200

    CSS voor iPad
    CSS voor iPad-formulier

--- index.php
+++ index.php
@@ -7,45 +7,53 @@
 ?>
 
 <!doctype html>
-<html lang="en" manifest="offline.manifest">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
     <link rel="stylesheet" type="text/css" href="css/waterlink.css">
-    <title>Registratie | Waterlink</title>
+    <title>registratie | water-link</title>
 </head>
 
 
 <body>
 
-<main class="content" role="main">
-    <div class="wrapper">
-        <form id="form_registration">
-            <label for="form_registration_input_event">event:</label>
-            <select name="form_registration_input_event" id="form_registration_input_event">
-                <?php
-                    foreach ($event->getAll() as $value){
-                        //extract values from array
-                        extract($value);
+<main class="container" role="main">
+    <div class="container_cell">
+        <div class="container_content">
 
-                        //show every option as option in select
-                        echo '<option>' . $name . '</option>';
-                    }
-                ?>
-            </select>
-            <label for="form_registration_input_email">email:</label>
-            <input type="email" name="form_registration_input_email" id="form_registration_input_email">
-            <label for="form_registration_input_conditions">ik aanvaard de algemene voorwaarden:</label>
-            <input type="checkbox" name="form_registration_input_conditions" id="form_registration_input_conditions" >
-            <input type="submit" name="submit" id="form_registration_input_submit">
+            <img src="assets/logo.png" alt="logo water-link" id="logo">
 
-            <p id="form_registration_message"></p>
-        </form>
+            <form id="form_registration" novalidate>
+                <label class="screenreader" for="form_registration_input_event">event:</label>
+                <select name="form_registration_input_event" id="form_registration_input_event">
+                    <?php
+                        foreach ($event->getAll() as $value){
+                            //extract values from array
+                            extract($value);
 
-        <div id="form_unsynced">
-            <a href="#" id="form_unsynced_show">niet-gesynchroniseerde adressen</a>
-            <ul id="form_unsynced_list"></ul>
+                            //show every option as option in select
+                            echo '<option>' . $name . '</option>';
+                        }
+                    ?>
+                </select>
+                <label class="screenreader" for="form_registration_input_email">email:</label>
+                <input type="text" name="form_registration_input_email" id="form_registration_input_email" placeholder="E-mail">
+                <div id="form_registration_div_conditions">
+                    <input type="checkbox" name="form_registration_input_conditions" id="form_registration_input_conditions" >
+                    <label for="form_registration_input_conditions">Ik aanvaard de algemene voorwaarden</label>
+                </div>
+                <input type="submit" name="submit" id="form_registration_input_submit" value="Registreer">
+
+                <p id="form_registration_message"></p>
+            </form>
+
+            <div id="form_unsynced">
+                <h2><a href="#" id="form_unsynced_show">Niet-gesynchroniseerde adressen</a></h2>
+                <ul id="form_unsynced_list"></ul>
+            </div>
         </div>
     </div>
 </main>
--- script/contact.js
+++ script/contact.js
@@ -11,6 +11,14 @@ $(function() {
         $form_unsynced_list.append("<li><a id='" + localStorage[i] +"' class='form_unsynced_list_item ' href='#'>" + localStorage[i] + "</a></li>");
     }
 
+    //set container to 100% window height
+    $('.container').css('height', $(window).height());
+
+    //also on resize
+    $(window).resize(function(){
+        $('.container').css('height', $(window).height());
+    });
+
     //remove status text
     $('body').on('click', $form_mail, function(e){
        $message.text("");
@@ -20,66 +28,72 @@ $(function() {
     $form.on("submit", function(e){
         e.preventDefault();
 
-        if(navigator.onLine){
-            if($('#form_registration_input_conditions').is(":checked")){
-                //serialize (key-value) the form data
-                var formdata = $($form).serialize();
+        if($('#form_registration_input_conditions').is(":checked")){
+            if(validEmail($form_mail.val())){
+                if(navigator.onLine){
+                    //serialize (key-value) the form data
+                    var formdata = $($form).serialize();
 
-                //submit form with ajax
-                $.ajax({
-                    type: 'POST',
-                    url: 'php/mailone.php',
-                    data: formdata,
-                    dataType: 'json'
-                }).done(function(response){
-                    if(response["result"]){
+                    //submit form with ajax
+                    $.ajax({
+                        type: 'POST',
+                        url: 'php/mailone.php',
+                        data: formdata,
+                        dataType: 'json'
+                    }).done(function(response){
+                        if(response["result"]){
+                            $message.removeClass('error');
+                            $message.addClass('success');
+                        }else{
+                            $message.removeClass('success');
+                            $message.addClass('error');
+                        }
+
+                        $message.text(response["message"]);
+
+                        //clear form after submission
+                        $form.each(function(){
+                            this.reset();
+                        });
+                    });
+                } else{
+                    if (typeof(Storage) !== "undefined") {
+                        // localstorage
+
+                        //get input email
+                        var email = $("#form_registration_input_email").val();
+                        var key = email;
+
+                        //set mail in local storage
+                        localStorage.setItem(key, email);
+
+                        //mail saved in local storage because offline
                         $message.removeClass('error');
                         $message.addClass('success');
-                    }else{
-                        $message.removeClass('success');
-                        $message.addClass('error');
+                        $message.text("Je bent offline, dit e-mailadres werd lokaal opgeslagen. Niet vergeten te syncen!");
+
+                        //append item to list
+                        $form_unsynced_list.append("<li><a id='" + localStorage.key(key) +"' class='form_unsynced_list_item ' href='#'>" + email + "</a></li>");
+
+                        //clear form after submission
+                        $form.each(function(){
+                            this.reset();
+                        });
+                    } else {
+                        // sorry! no web storage support..
                     }
-
-                    $message.text(response["message"]);
-
-                    //clear form after submission
-                    $form.each(function(){
-                        this.reset();
-                    });
-                });
+                }
             }else{
                 //set error message
                 $message.removeClass('success');
                 $message.addClass('error');
-                $message.text('Gelieve de algemene voorwaarden te accepteren');
+                $message.text('Gelieve een geldig e-mailadres in te geven');
             }
-        }
-        else{
-            if (typeof(Storage) !== "undefined") {
-                // localstorage
-
-                //get input email
-                var email = $("#form_registration_input_email").val();
-                var key = email;
-
-                //set mail in local storage
-                localStorage.setItem(key, email);
-
-                //mail saved in local storage because offline
-                $message.removeClass('error');
-                $message.addClass('success');
-                $message.text("Je bent offline, dit mailadres werd tijdelijk in local storage opgeslagen. Niet vergeten te syncen!");
-
-                //append item to list
-                $form_unsynced_list.append("<li><a id='" + localStorage.key(key) +"' class='form_unsynced_list_item ' href='#'>" + email + "</a></li>");
-
-                //clear form after submission
-                $form.each(function(){
-                    this.reset();
-                });
-            } else {
-                // sorry! no web storage support..
-            }
+        }else{
+            //set error message
+            $message.removeClass('success');
+            $message.addClass('error');
+            $message.text('Gelieve de algemene voorwaarden te accepteren');
         }
     });
 
@@ -87,7 +101,7 @@ $(function() {
     $form_unsynced_link.on("click", function(e){
         e.preventDefault();
 
-        $form_unsynced_list.toggle("slow", function(){
+        $form_unsynced_list.toggle("fast", function(){
 
         });
     });
@@ -112,3 +126,8 @@ $(function() {
         $(this).remove();
     });
 });
+
+function validEmail(input) {
+    var reg= new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+    return (input.match(reg) == null) ? false : true;
+}
